581dc492866fd3f92f6b3d71700aba0e980c863e
Ricardo Fernando Saidel <rfsaidel@gmail.com>
2021-07-25 11:28:43 -0300

    Update release configuration

--- app/src/main/java/com/saidel/bookdex/utils/Constants.kt
+++ app/src/main/java/com/saidel/bookdex/utils/Constants.kt
@@ -2,12 +2,11 @@ package com.saidel.bookdex.utils
 
 class Constants {
     companion object {
-        val API_URL_PKM_LIST = "https://pokeapi.co/api/v2/pokemon?limit=898"
-        val PKM_NUMBER = "pkm_number"
-        val PKM_NAME = "pkm_name"
-        val PKM_URL = "pkm_url"
+        const val API_URL_PKM_LIST = "https://pokeapi.co/api/v2/pokemon?limit=898"
+        const val PKM_NUMBER = "pkm_number"
+        const val PKM_NAME = "pkm_name"
 
-        val API_URL_PKM_DETAILS = "https://pokeapi.co/api/v2/pokemon/"
-        val API_URL_PKM_MORE_DETAILS = "http://pokeapi.co/api/v2/pokemon-species/"
+        const val API_URL_PKM_DETAILS = "https://pokeapi.co/api/v2/pokemon/"
+        const val API_URL_PKM_MORE_DETAILS = "http://pokeapi.co/api/v2/pokemon-species/"
     }
 }
